7eb1b72c619c88e61a1f5bdbbc6fdfb7d5af2b9a
oaps93 <96922230+oaps93@users.noreply.github.com>
2022-10-10 11:03:33 -0500

    Updating searchPalindrome

--- searchPalindrome.java
+++ searchPalindrome.java
@@ -1,20 +1,24 @@
+import java.util.ArrayList;
+
 public class searchPalindrome {
 
     public static void main(String[] args){
 
-        String s = new String("aaaa");
+        String s = new String("aaaaaaaa");
         int palindromeCount = 0;
         int len = s.length();
+        ArrayList<String> wordsToTest = new ArrayList<>();
         
 
         for(int i = 0; i < len; i++){
             for ( int j = i+1; j <= len; j++){
-                if (isPalindrome(s.substring(i,j))){
-                    palindromeCount++;                    
-                }
+                wordsToTest.add(s.substring(i,j));
             }
-            
         } 
+
+        for(int i = 0; i < wordsToTest.size(); i++){
+            if(isPalindrome(wordsToTest.get(i))) palindromeCount++;
+        }
 
         System.out.println(palindromeCount);
         
