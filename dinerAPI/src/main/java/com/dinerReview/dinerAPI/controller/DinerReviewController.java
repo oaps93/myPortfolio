aa80f191bc68e25fa897eeb3f6524ff4086db4a0
oaps_asus <oaps93@gmail.com>
2022-12-12 22:09:11 -0600

    Still updating task 13

--- dinerAPI/src/main/java/com/dinerReview/dinerAPI/controller/DinerReviewController.java
+++ dinerAPI/src/main/java/com/dinerReview/dinerAPI/controller/DinerReviewController.java
@@ -6,14 +6,13 @@ import com.dinerReview.dinerAPI.model.User;
 import com.dinerReview.dinerAPI.repository.DiningReviewRepository;
 import com.dinerReview.dinerAPI.repository.RestaurantRepository;
 import com.dinerReview.dinerAPI.repository.UserRepository;
+import net.bytebuddy.dynamic.DynamicType;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.Entity;
+import javax.swing.text.html.Option;
 import java.util.DuplicateFormatFlagsException;
 import java.util.List;
 import java.util.Optional;
@@ -47,6 +46,29 @@ public class DinerReviewController {
         return  restaurant;
     }
 
+    @PostMapping("/restaurant")
+    public Restaurant createRestaurant(@RequestBody Restaurant restaurant){
+        Restaurant newRestaurant = this.restaurantRepository.save(restaurant);
+        return newRestaurant;
+    }
+
+    @PutMapping("/restaurant/{id}")
+    public Restaurant editRestaurant(@RequestBody Restaurant restaurant,@PathVariable Long id){
+        Optional<Restaurant> restaurantOptional = this.restaurantRepository.getById(id);
+        if(restaurantOptional.isEmpty()){
+            return null;
+        }
+        Restaurant restaurantToUpdate = restaurantOptional.get();
+        if(restaurant.getName()!= null){
+            restaurantToUpdate.setName(restaurant.getName());
+        }
+        if(restaurant.getZipCode()!= null){
+            restaurantToUpdate.setName(restaurant.getZipCode());
+        }
+        return this.restaurantRepository.save(restaurantToUpdate);
+
+    }
+
     @GetMapping("/user")
     public Iterable<User> getAllUsers(){
         return this.userRepository.findAll();
@@ -60,6 +82,12 @@ public class DinerReviewController {
         }
         User user = userOptional.get();
         return user;
+    }
+
+    @PostMapping("user")
+    public User createUser(@RequestBody User user){
+        User newUser = this.userRepository.save(user);
+        return newUser;
     }
 
     @GetMapping("/diningReview")
@@ -77,6 +105,12 @@ public class DinerReviewController {
         DiningReview diningReview = diningReviewOptional.get();
         return  diningReview;
 
+    }
+
+    @PostMapping("/diningReview")
+        public DiningReview createDiningReview(@RequestBody DiningReview diningReview){
+        DiningReview newDiningReview = this.diningReviewRepository.save(diningReview);
+        return newDiningReview;
     }
 
 
