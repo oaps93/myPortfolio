9910df0af32fbe32cf696cbd7f78042680458778
oaps93_surfacehome <oaps93@gmail.com>
2023-05-25 15:19:07 -0600

    updating resume

---
+++ DanceApp/DanceApp/src/main/java/com/dance/DanceApp/DanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.dance.DanceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceAppApplication.class, args);
+	}
+
+}
